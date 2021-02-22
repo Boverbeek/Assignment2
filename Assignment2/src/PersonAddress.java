7fd8992b4c30258ca1c8ec92342b77b1d19e71b2
shamacher <69808455+shamacher@users.noreply.github.com>
2021-02-22 16:50:41 -0600

    Added Store and storeaddress classes

--- Assignment2/src/PersonAddress.java
+++ Assignment2/src/PersonAddress.java
@@ -1,12 +1,12 @@
 
-public class Address {
+public class PersonAddress {
 	String street;
 	String city;
 	String state;
 	String zipcode;
 	String country;
 	
-	public Address (String street, String city, String state, String zipcode, String country) {
+	public PersonAddress (String street, String city, String state, String zipcode, String country) {
 		
 		this.street = street;
 		this.city = city;
