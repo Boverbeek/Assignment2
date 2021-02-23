d5ba67ddc7c826cfe215d6b2065ea5ca26860d66
Boverbeek <69643015+Boverbeek@users.noreply.github.com>
2021-02-23 14:46:23 -0600

    Added Files to dataConverter

--- Assignment2/src/DataConverter.java
+++ Assignment2/src/DataConverter.java
@@ -11,13 +11,36 @@ import java.util.Scanner;
  *
  */
 public class DataConverter {
+	
+	private static final String Persons_File = "data/Persons.csv";
+	
+	private static final String Items_File = "data/Items.csv";
+	
+	private static final String Stores_File = "data/Stores.csv";
+	
+	private static final String Sales_File = "data/Sales.csv";
 
 	public static void main(String[] args) {
 		
 		try {
 			
-			Scanner inputFile = new Scanner(new File(args[0]));
-			while (inputFile.hasNextLine()) {
+			Scanner personsFile = new Scanner(new File(Persons_File));
+			while (personsFile.hasNextLine()) {
+				
+			}
+			
+			Scanner itemsFile = new Scanner(new File(Items_File));
+			while (itemsFile.hasNextLine()) {
+				
+			}
+			
+			Scanner storesFile = new Scanner(new File(Stores_File));
+			while (storesFile.hasNextLine()) {
+				
+			}
+			
+			Scanner salesFile = new Scanner(new File(Sales_File));
+			while (salesFile.hasNextLine()) {
 				
 			}
 			
