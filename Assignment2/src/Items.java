9d915e314a46981879963963d795d10767828f65
shamacher <69808455+shamacher@users.noreply.github.com>
2021-02-23 14:41:52 -0600

    added items class

--- Assignment2/src/Items.java
+++ Assignment2/src/Items.java
@@ -1,5 +1,13 @@
 
 public class Items {
+	Product product;
+	Services services;
+	Subscriptions subscriptions;
 	
 	
+	public Items (Product product, Services services, Subscriptions subscriptions) {
+		this.product = product;
+		this.services = services;
+		this.subscriptions = subscriptions;
+	}
 }
